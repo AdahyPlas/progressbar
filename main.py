96d4304219f165853aa7fb6f2b2299d0eabb371f
Adahy <59096820+adahyplas@users.noreply.github.com>
2020-12-19 09:32:35 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from time import sleep #importation du module Time
-from tqdm import tqdm #importation du module tqdm
-
-def progressbar(x): #creation de la fonction "prgressbar"
-    for i in tqdm(range(x)): #boucle pour que le barre se mette a jour
-        sleep(3)
-
